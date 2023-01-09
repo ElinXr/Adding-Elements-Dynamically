94e9a30cb926aafa1eb6c20e4a11e5c844779fa6
Elin Xristozov <73062722+ElinXr@users.noreply.github.com>
2023-01-09 15:41:12 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,13 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  window.addEventListener("load", function() {
+    alert("Hey!");
+  })
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
-   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
-   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
-   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
-   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
-  
+     
 });
